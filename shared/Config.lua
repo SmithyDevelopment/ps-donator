1d05d2fb62b127967102fdec8e88d79f9e0e3d87
SmithyDevelopment <95915864+SmithyDevelopment@users.noreply.github.com>
2022-12-08 19:21:04 +0000

    Update Config.lua

--- shared/Config.lua
+++ shared/Config.lua
@@ -1,18 +1,18 @@
 Config = {}
 
-Config.NPC = vector4(185.29, -916.61, 30.69, 148.52) -- Location for the NPC to spawn.
+Config.NPC = vector4(-1266.99, -3013.06, -48.49, 187.41) -- Location for the NPC to spawn.
 Config.Model = "cs_fbisuit_01" -- NPC Model.
-Config.VehicleSpawn = vector4(180.45, -923.15, 30.69, 230.33) -- Location for car to spawn.
-Config.Garage = "garage1" -- Default garage for the car to be tagged too.
+Config.VehicleSpawn = vector4(-1266.98, -3002.33, -47.49, 179.84) -- Location for car to spawn.
+Config.Garage = "legionsquare" -- Default garage for the car to be tagged too.
 
 Config.CoinShop = {
     [1] = {
-        type = "item", -- Item/Car
-        name = "weapon_pistol", -- Name of Item.
+        type = "car", -- Item/Car
+        name = "voltic", -- Name of Item.
         amount = 1, -- amount of items to give.
         cost = 1, -- Coin Cost to purchase item.
-        header = "Buy Pistol", -- Header Text.
-        text = "Buy pistol for 1 coin.", -- Sub Text.
+        header = "Buy Voltic", -- Header Text.
+        text = "Buy Voltic for 1 coin.", -- Sub Text.
     },
     [2] = {
         type = "car", -- Item/Car
@@ -20,6 +20,35 @@ Config.CoinShop = {
         cost = 1, -- Coin Cost to purchase item.
         header = "Buy Sultan", -- Header Text.
         text = "Buy a Sultan for 1 coin.", -- Sub Text.
+    },
+	[3] = {
+        type = "car", -- Item/Car
+        name = "brioso", -- Model of the vehicle to spawn.
+        cost = 1, -- Coin Cost to purchase item.
+        header = "Buy Brioso", -- Header Text.
+        text = "Buy a Brioso for 1 coin.", -- Sub Text.
+    },
+	[4] = {
+        type = "car", -- Item/Car
+        name = "voltic", -- Name of Item.
+        amount = 1, -- amount of items to give.
+        cost = 1, -- Coin Cost to purchase item.
+        header = "Buy Voltic", -- Header Text.
+        text = "Buy Voltic for 1 coin.", -- Sub Text.
+    },
+    [5] = {
+        type = "car", -- Item/Car
+        name = "sultan", -- Model of the vehicle to spawn.
+        cost = 1, -- Coin Cost to purchase item.
+        header = "Buy Sultan", -- Header Text.
+        text = "Buy a Sultan for 1 coin.", -- Sub Text.
+    },
+	[6] = {
+        type = "car", -- Item/Car
+        name = "brioso", -- Model of the vehicle to spawn.
+        cost = 1, -- Coin Cost to purchase item.
+        header = "Buy Brioso", -- Header Text.
+        text = "Buy a Brioso for 1 coin.", -- Sub Text.
     },
 }
 
